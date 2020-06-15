0daa1166166cdbaab44f16f32bc3727c7cf12de9
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2020-06-15 15:43:45 -0300

    Modulos com mesmo nome

---
+++ pacotes/pacote_v2.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3
+from pacote1 import modulo2
+
+modulo2.main()
---
+++ pacotes/pacote_v3.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+from pacote1 import modulo1
+from pacote2 import modulo1 as modulo1_sub
+
+print('Soma', modulo1.soma(3, 2))
+print('Subtração', modulo1_sub.subtracao(3, 2))
