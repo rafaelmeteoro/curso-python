98200ed10ec406ca0f6bd69cdd146965cd6e4345
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2020-06-20 15:48:49 -0300

    Pacotes como facades

---
+++ pacotes/pacote_v4.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+from pacote1.modulo1 import soma
+from pacote2.modulo1 import subtracao as sub
+
+print('Soma', soma(3, 2))
+print('Subtração', sub(3, 2))
---
+++ pacotes/pacote_v5.py
@@ -0,0 +1,5 @@
+#!/usr/local/bin/python3
+from calc import soma, subtracao
+
+print('Soma', soma(3, 2))
+print('Subtracao', subtracao(3, 2))
