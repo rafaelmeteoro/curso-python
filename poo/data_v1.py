daa26e11041d0b70473ef932d3efa7e882f2f65c
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2020-06-21 12:19:16 -0300

    Desafio carro

--- poo/data_v1.py
+++ poo/data_v1.py
@@ -1,17 +1,22 @@
 #!/usr/local/bin/python3
 class Data:
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
     def __str__(self):
         return f'{self.dia}/{self.mes}/{self.ano}'
 
 
-d1 = Data()
-d1.dia = 5
-d1.mes = 12
-d1.ano = 2019
+d1 = Data(5, 12, 2019)
+# d1.dia = 5
+# d1.mes = 12
+# d1.ano = 2019
 print(d1)
 
-d2 = Data()
-d2.dia = 7
-d2.mes = 11
-d2.ano = 2020
+d2 = Data(7, 11, 2020)
+# d2.dia = 7
+# d2.mes = 11
+# d2.ano = 2020
 print(d2)
